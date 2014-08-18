cd24571f4185a6e7f22c865e319a8b94bf7de605
atmman <kill.cops.381@gmail.com>
2014-08-18 11:48:22 +0400

    0.01

--- add_report_to_netmap.py
+++ add_report_to_netmap.py
@@ -1,5 +1,6 @@
 from netaddr import IPNetwork
 
+
 def combine_netmap_report(netmap, report_dict):
     for subnet in netmap.subnets:
         for net_object in subnet.net_objects:
@@ -11,34 +12,37 @@ def combine_netmap_report(netmap, report_dict):
                 host_dict = report_dict.get(str(interface.cidr.ip))
 
                 for cpe_item in host_dict.get('cpe_list'):
-                    cpe = cpe_item['cpe']
-                    port = cpe_item.get('port')
-                    vulns = get_vulnerabilities(host_dict, cpe)
-                    add_service(net_object, cpe, port, vulns)
+                    new_service = add_service(net_object, cpe_item)
+                    new_service.vulnerabilities = get_vulnerabilities(host_dict['cve_list'], new_service.cpe)
 
                 for cve_item in host_dict.get('cve_list'):
                     if not cve_item.get('possible_cpe'):
                         continue
                     if cve_item.get('possible_cpe') in [service.cpe for service in net_object.services]:
                         continue
-                    cpe = cve_item['possible_cpe']
-                    port = cve_item.get('port')
-                    vulns = get_vulnerabilities(host_dict, cpe)
-                    add_service(net_object, cpe, port, vulns)
+
+                    add_service(net_object, cve_item)
+                    new_service.vulnerabilities = get_vulnerabilities(host_dict['cve_list'], new_service.cpe)
+
 
     return netmap
 
-def add_service(net_object, cpe, port, vulns):
+
+def add_service(net_object, item):
+    cpe = item.get('cpe') or item.get('possible_cpe')
+    port = item['port']
+
     service = net_object.add_service()
     service.cpe = cpe
     service.port = port
-    service.vulnerabilities = vulns
 
     return service
 
-def get_vulnerabilities(host_dict, cpe):
+def get_vulnerabilities(cve_list, cpe):
     vulns = [{'cve':cve_item.get('cve'), 'source':cve_item.get('source_type')}
-             for cve_item in host_dict.get('cve_list')
+             for cve_item in cve_list
              if cpe == cve_item.get('cpe') or cpe == cve_item.get('possible_cpe')]
 
     return vulns
+
+
